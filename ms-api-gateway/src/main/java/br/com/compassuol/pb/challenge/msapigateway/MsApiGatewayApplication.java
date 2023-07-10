abb638a3e17f5a94c9e38d3d04cfce93645a0ea9
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2023-07-10 17:31:03 -0300

    feat: api gateway microservice creation

---
+++ ms-api-gateway/src/main/java/br/com/compassuol/pb/challenge/msapigateway/MsApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.com.compassuol.pb.challenge.msapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsApiGatewayApplication.class, args);
+	}
+
+}
